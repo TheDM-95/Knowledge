146125a185c5c99d93f767d17079e5b3eb32cdd8
NguyenQuocHung <qhungnb@gmail.com>
2015-09-26 21:01:26 +0700

    first commit

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->increments('user_id');
+            $table->rememberToken();
+
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('user_name');
+            $table->string('password');
+            $table->boolean('is_active')->nullable();
+            $table->bigInteger('score');
+            $table->string('gender');
+            $table->string('avatar');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('users');
+    }
+}
---
+++ database/migrations/2015_09_22_124647_create_questions_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateQuestionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('questions', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->increments('question_id');
+
+            $table->text('content');
+            $table->integer('cat_id');
+            $table->boolean('is_active')->nullable();
+            $table->smallInteger('level');
+            $table->string('image');
+
+            $table->timestamps('created_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('questions');
+    }
+}
