e8f1ff3d95b646eb5658505dc5bce1c271536dab
NguyenVanVu-K58CA <vunv_58@vnu.edu.vn>
2015-10-11 17:05:17 +0700

    question version 2 with fade data

--- resources/views/pages/category.blade.php
+++ resources/views/pages/category.blade.php
@@ -8,14 +8,19 @@
    <div class="theader"><a href="back">Back</a></div>
    <!-- Body-->
    <div  class="tbody">
-            <div ng-repeat="items in data">
+            <div ng-repeat="items in data" ng-init='index1 = $index' ng-show='index1 == count()'>
                 <!--Question area -->
                 <h2>{{items.questions}}</h2>
                 <!--Answer area -->
-                <div ng-repeat="item in items.answers">
-                    <input type='radio' ng-click='choise_click(item.choise,items.correct)'> 
-                    <h4>{{item.choise}} : {{item.ans}}</h4> 
+                <div ng-repeat="item in items.answers" ng-init='index2 = $index'>
+                  <label>
+                    <input type='radio' ng-click='choise_click(index1, index2)'> 
+                    <h4>{{item.choise}} : {{item.ans}}</h4>
+                  </label>
+                  
                 </div>
+                <!--button summit appear when user chose a choise -->
+                <input type='summit' value='summit' ng-click='summit()' ng-show='isShow==true' />
             </div> 
    </div>
    <!-- Score area -->
@@ -36,22 +41,34 @@
                 } ,
                 {
                   questions : 'How did you know ?',
-                   answers : [{choise: 'A', ans : 'yes, i did' },{choise: 'B', ans : 'no, i did not'}],
+                   answers : [{choise: 'A', ans : 'yes, i did ! Yahoo' },{choise: 'B', ans : 'no, i did not Whoope'}],
                   correct: 'A' 
                 }
             ];
              $scope.data = database;
-             var _score = 0;
-            $scope.choise_click = function(item_choise,item_ans){
-                    if ((item_choise) &&(item_ans) &&(item_choise == item_ans)) _score++;
+             var _score = 0, check = 0;
+             $scope.isShow = false;
+            $scope.choise_click = function(question,answer){
+                    if ($scope.data[question].answers[answer].choise == $scope.data[question].correct )
+                    {
+                            _score++;
+                    }
+                    $scope.isShow = $scope.isShow ? false : true;
+                   // alert($scope.isShow);
              }
-
-
             $scope.incompleteCount = function(){
                   
                     return _score;
             }
 
+            $scope.count = function(){
+                    return check;
+            }
+
+            $scope.summit = function(){
+                   check++;
+                  $scope.isShow = $scope.isShow ? false: true;
+            }
         });
 	</script>
 @stop
