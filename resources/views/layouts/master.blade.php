2067a0abd34b7fbc4c7042549eb0af99152fd8f8
NguyenVanVu-K58CA <vunv_58@vnu.edu.vn>
2015-10-12 11:03:07 +0700

    work with firebaseIO

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -8,6 +8,8 @@
     <link rel="stylesheet" href="font-awesome-4.3.0/css/font-awesome.min.css">
     <script src="js/jquery.min.js"></script>
     <script type="text/javascript" src="js/shared/angular.min.js"></script>
+    <script src='https://cdn.firebase.com/libs/angularfire/1.1.1/angularfire.min.js'></script>
+    <script src='https://cdn.firebase.com/js/client/2.2.2/firebase.js'></script>
     @yield("head.style")
 
     <script src="js/jquery.min.js"></script>
